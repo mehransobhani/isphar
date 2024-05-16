38c9eba33ae5b5937a750923038da799c4d6f440
mehran <mehransobhani77@gmail.com>
2024-05-16 18:56:27 +0330

    removeExtraFiles

--- resources/views/admin/datatables/action.blade.php
+++
@@ -1,3 +0,0 @@
-<a href="{{ $route }}" class="btn btn-block btn-outline btn-primary btn-rounded waves-effect">
-  مدیریت
-</a>
--- resources/views/admin/datatables/image.blade.php
+++
@@ -1,4 +0,0 @@
-<img src="{{ route('file.get', [$file_id]) }}"
-     data-image="{{ route('file.get', [$file_id]) }}"
-     class="showImagePrevModal h-100 rounded-3 aspect-ratio:1/1 cursor-pointer"
-     style="width: 50px;">
