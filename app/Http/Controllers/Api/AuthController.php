9b9239e51e68b44b7bf796948ef2fdac1ba1e653
Your Name <you@example.com>
2024-05-26 16:45:06 +0330

    AuthController

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -18,21 +18,18 @@ class AuthController extends Controller
             'mobile' => 'required',
             'password' => 'required|string',
         ]);
-
         if ($validator->fails()) {
-            return response()->json(['error' => $validator->errors()], 422);
+            $this->apiResponse(['error' => $validator->errors()], 422);
         }
         $credentials = $request->only('mobile', 'password');
-
         if (Auth::attempt($credentials)) {
             $user = Auth::user();
             $token = $user->createToken('auth-token')->plainTextToken;
-            return response()->json(['token' => $token], 200);
+            $this->apiResponse(['token' => $token]);
         } else {
-            return response()->json(['error' => 'Unauthorized'], 401);
+            $this->apiResponse(['error' => 'Unauthorized'], 401);
         }
     }
-
 
     public function register(Request $request)
     {
@@ -41,56 +38,59 @@ class AuthController extends Controller
             'mobile' => 'required|string|max:255|unique:users',
             'password' => 'required|string',
         ]);
-
         if ($validator->fails()) {
-            return response()->json(['error' => $validator->errors()], 422);
+            $this->apiResponse(['error' => $validator->errors()], 422);
         }
-
         $user = User::create([
             'name' => $request->name,
             'mobile' => $request->email,
             'created_at' => date("Y-m-d H:i:s", time()),
             'password' => bcrypt($request->password),
         ]);
-
         $token = $user->createToken('Api')->accessToken;
-        return response()->json(['token' => $token], 201);
+        $this->apiResponse(['token' => $token], 201);
     }
 
     public function forgotCodeVerify(Request $request)
     {
-        $mobile=$request->mobile;
-        $requestCode=$request->code;
-        $user = User::where("mobile",$mobile)->first();
-        $code=$user->reset_pwd_code;
-        if($requestCode==$code)
-        {
-            return $this->apiResponse("success");
+        $validator = Validator::make($request->all(), [
+            'mobile' => 'required|string',
+            'code' => 'required|string',
+        ]);
+        if ($validator->fails()) {
+            $this->apiResponse(['error' => $validator->errors()], 422);
         }
-        return $this->apiResponse("code not valid",400);
+        $mobile = $request->mobile;
+        $requestCode = $request->code;
+        $user = User::where("mobile", $mobile)->first();
+        $code = $user->reset_pwd_code;
+        if ($requestCode == $code) {
+            return $this->apiResponse(['message' => "completed"]);
+        }
+        return $this->apiResponse(['message' => "The code is not valid"], 400);
     }
 
     public function resetPassword(Request $request)
     {
-        $newPassword=$request->newPassword;
-        $code=$request->code;
-        $mobile=$request->mobile;
-        $user = User::where("mobile",$mobile)->where("reset_pwd_code",$code)->first();
-        if(!$user)
-            return $this->apiResponse("not find",400,"user not find");
-        $user->update(["password"=>bcrypt($newPassword)]);
-        return $this->apiResponse("success");
+        $newPassword = $request->newPassword;
+        $code = $request->code;
+        $mobile = $request->mobile;
+        $user = User::where("mobile", $mobile)->where("reset_pwd_code", $code)->first();
+        if (!$user)
+            return $this->apiResponse(['message' => "The user not find"], 400);
+        $user->update(["password" => bcrypt($newPassword)]);
+        return $this->apiResponse(['message' => "completed"]);
     }
 
     public function forgot(Request $request)
     {
         $code = rand(10000, 99999);
         $mobile = $request->mobile;
-        $user = User::where("mobile",$mobile)->first();
-        if(!$user)
-            return $this->apiResponse("not find",400,"user not find");
-        $user->update(["reset_pwd_code"=>$code , "reset_pwd_date"=>date("Y-m-d H:i:s", time())]);
-        Sms::send($user->mobile,$code);
-        return $this->apiResponse(["message"=>"sended"]);
+        $user = User::where("mobile", $mobile)->first();
+        if (!$user)
+            return $this->apiResponse(['message' => "The user not find"], 400);
+        $user->update(["reset_pwd_code" => $code, "reset_pwd_date" => date("Y-m-d H:i:s", time())]);
+        Sms::send($user->mobile, $code);
+        return $this->apiResponse(['message' => "completed"]);
     }
 }
