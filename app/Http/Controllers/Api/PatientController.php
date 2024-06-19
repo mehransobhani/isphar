c0e12896d73297397d69439207b5c1459398d2a3
farid <farid0121@gmail.com>
2024-06-19 17:34:19 +0330

    init

--- app/Http/Controllers/Api/PatientController.php
+++ app/Http/Controllers/Api/PatientController.php
@@ -7,6 +7,7 @@ use App\Http\Controllers\Controller;
 use App\Models\Patient;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
+use Artisan;
 
 class PatientController extends Controller
 {
