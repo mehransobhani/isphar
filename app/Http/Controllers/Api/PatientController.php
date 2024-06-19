d70324045f5c7b73beab12a21c5e6a8c88754739
farid <farid0121@gmail.com>
2024-06-19 17:24:16 +0330

    init

--- app/Http/Controllers/Api/PatientController.php
+++ app/Http/Controllers/Api/PatientController.php
@@ -88,7 +88,6 @@ class PatientController extends Controller
 
     public function update(Request $request)
     {
-        dd($request->id);
         $validator = Validator::make($request->all(), [
             'id' => 'required',
             'fullname' => 'optional|string',
