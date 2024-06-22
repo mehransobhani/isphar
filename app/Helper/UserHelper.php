4d2ec755df43bfabf898c875d92e5d716c8bdf3f
farid <farid0121@gmail.com>
2024-06-22 14:40:36 +0330

    debug

--- app/Helper/UserHelper.php
+++ app/Helper/UserHelper.php
@@ -8,11 +8,10 @@ function myUser()
     return auth()->user();
 }
 function removeNullFields($array){
-    var_dump($array);
     foreach($array as $key=>$value){
         if($value=="null"){
             unset($array[$key]);
         }
     }
-    return array_values($array);
+    return $array;
 }
