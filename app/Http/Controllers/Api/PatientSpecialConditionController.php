1576ca49e17debde301ffa023d0899ece0cec7d1
farid <farid0121@gmail.com>
2024-06-22 14:30:35 +0330

    debug

--- app/Http/Controllers/Api/PatientSpecialConditionController.php
+++ app/Http/Controllers/Api/PatientSpecialConditionController.php
@@ -64,7 +64,7 @@ class PatientSpecialConditionController extends Controller
         if ($validator->fails()) {
             return $this->apiResponse(['error' => $validator->errors()], 422);
         }
-        $inputs = array_filter($linksArray->all());
+        $inputs = array_filter($request->all());
         var_dump($inputs);
         PatientSpecialCondition::where("id", $request->id)->update($inputs);
         return $this->apiResponse(["message" => "Completed"]);
