6ece1733c3db7ac7e8de010423ec232c0cbb317b
farid <farid0121@gmail.com>
2024-06-27 22:21:17 +0330

    debug

--- app/Http/Controllers/Api/DrpReportController.php
+++ app/Http/Controllers/Api/DrpReportController.php
@@ -25,7 +25,7 @@ class DrpReportController extends Controller
             ->latest("id")->get();
         }else if($request->page != -1 && $request->page != null){
             $drpReport = DrpReport::with("patient")
-            ->join("users", "users.id", "=", "drp_reports.user_id")->latest("id")->paginate();
+            ->join("users", "users.id", "=", "drp_reports.user_id")->latest("drp_reports.id")->paginate();
         }else if(isset($request->patient_id)){
             $drpReport = DrpReport::with(["patient"=>function ($query) {
                 $query->with(["patientDrug"=>function($query){
