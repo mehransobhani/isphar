385be70d35d427b4ee10eb821b4056d3c48530f2
farid <farid0121@gmail.com>
2024-06-27 22:29:37 +0330

    debug

--- app/Http/Controllers/Api/DrpReportController.php
+++ app/Http/Controllers/Api/DrpReportController.php
@@ -45,7 +45,9 @@ class DrpReportController extends Controller
     }
     public function find($id)
     {
-        $drpReport = DrpReport::with(["patient"=>function ($query) {
+        $drpReport = DrpReport::join("users", "users.id", "=", "drp_reports.user_id")
+        ->where("users.id", userId())->latest("drp_reports.id")
+        ->with(["patient"=>function ($query) {
             $query->with(["patientDrug"=>function($query){
                 $query->with(["drug"=>function($query){
                     $query->select("drugs.*");
@@ -53,8 +55,7 @@ class DrpReportController extends Controller
             }])
             ->with(["PatientSpecialCondition"]);
         }])
-        ->join("users", "users.id", "=", "drp_reports.user_id")
-        ->where("users.id", userId())->latest("drp_reports.id")->where("drp_reports.id",$id);
+        ->where("drp_reports.id",$id);
         return $this->apiResponse(["data" => $drpReport]);
     }
 
